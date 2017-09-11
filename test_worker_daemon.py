23e0c4c96c90cadd96ce215b6387f4a8c9af48a5
apache8080 <desai.rishi1@gmail.com>
2017-09-10 20:17:09 -0500

    cleaned up some of the code

--- test_worker_daemon.py
+++ test_worker_daemon.py
@@ -5,11 +5,10 @@ import docker
 client = docker.from_env()
 clearwaters_containers = []
 
-def create_container(cmd):
+def create_container():
     c = client.containers.run('registry.gitlab.com/acm-uiuc/sigops/clearwaters-docker/ubuntu-mpich-arm64', detach=True)
     clearwaters_containers.append(c)
 
 for i in range(3):
-    cmd = 'echo hello world'+str(i)
     create_container(cmd)
     clearwaters_containers[i].logs()
