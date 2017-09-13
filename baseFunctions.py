9ddbdbef2bc8cd0eb95c89b0276d46b5d217fbde
Gaurav Desale <desalegaurav60@gmail.com>
2017-09-13 09:15:58 -0500

    Delete baseFunctions.py

--- baseFunctions.py
+++
@@ -1,7 +0,0 @@
-import docker
-client = docker.from_env()
-#go to tanishq next time and ask about the conditions for the containers
-def morePrivilage(cmd):
-    c = client.containers.cap_add()
-    return c.id
-
