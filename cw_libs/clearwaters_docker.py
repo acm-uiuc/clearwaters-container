0c02566af6b15918ea45bba9dcc13aa836650994
apache8080 <desai.rishi1@gmail.com>
2017-09-20 14:47:50 -0500

    fixed docker version bug

--- cw_libs/clearwaters_docker.py
+++ cw_libs/clearwaters_docker.py
@@ -6,7 +6,8 @@ class CWDockerClient:
     client = None
     
     def __init__(self):
-        self.client = docker.from_env('auto')
+        self.client = docker.from_env()
+        self.client.version = 'auto'
         
     def create_container(self, cmd):
         c = self.client.containers.run('registry.gitlab.com/acm-uiuc/sigops/clearwaters-docker/ubuntu-mpich-arm64', cmd, detach=True)
